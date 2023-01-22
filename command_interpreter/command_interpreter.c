a5411f8afa12b2617c211891d15c0f823ddac7c3
ivayla44 <61330504+ivayla44@users.noreply.github.com>
2023-01-22 10:13:01 +0200

    Fixing the false returning 0 issue
    Adding (char*)'/0' as the last element of parsed**. I don't know how it worked on linux, but it was inherently an issue with execvp().

---
+++ command_interpreter/command_interpreter.c
@@ -0,0 +1,177 @@
+#include "error_handling.h"
+#include <sys/wait.h>
+
+int str_len(const char* str) {
+    int i = 0;
+    for(; str[i]; i++);
+    return i;
+}
+
+int get_lines(int fd) {
+    int ch_sz = 32, check_read = 1, res = 0;
+    char chunk[ch_sz];
+    while (1) {
+        check_read = err_read(fd, &chunk, ch_sz);
+        if (!check_read) break;
+        if (check_read == -1) {
+            exit(7);
+        }
+        for (int i = 0; i < check_read; i++) {
+            if (chunk[i] == '\n') res++;
+        }
+    }
+    return res;
+}
+
+char* readline(int fd) {
+    int chunk_sz = 16, check_read = 1, check_lseek, chunk_num = 0;
+    char* line = malloc(chunk_sz);
+    if(!line) {
+        printf("Malloc error.");
+        exit(1);
+    }
+    char* read_buf = malloc(chunk_sz);
+    if(!read_buf) {
+        printf("Malloc error.");
+        free(line);
+        exit(2);
+    }
+    while(check_read) {
+        check_read = err_read(fd, read_buf, chunk_sz);
+        if(check_read == -1) {
+            free(line);
+            free(read_buf);
+            exit(3);
+        }
+        if(!check_read) {
+            break;
+        }
+
+        for(int i = 0; i < check_read; i++) {
+            if(read_buf[i] != '\n') {
+                line[chunk_sz*chunk_num + i] = read_buf[i];
+            }
+            else {
+                check_lseek = err_lseek(fd, i - check_read + 1, SEEK_CUR);
+                if(check_lseek == -1) {
+                    free(line);
+                    free(read_buf);
+                    exit(4);
+                }
+                check_read = 0;
+                break;
+            }
+        }
+        line = realloc(line, chunk_sz*(chunk_num+2));
+        if(!line) {
+            printf("Realloc error.");
+            free(read_buf);
+            exit(5);
+        }
+        chunk_num++;
+    }
+    free(read_buf);
+    int line_sz = str_len(line);
+    line[line_sz] = '\0';
+    return line;
+}
+
+char** parseline(char* line) {
+    int line_sz = str_len(line);
+
+    char** parsed = malloc(sizeof(char*));
+    if(!parsed) {
+        free(line);
+        printf("Malloc error.");
+        exit(6);
+    }
+    // the idea is to malloc everything first and then fill in the symbols
+    for(int word_sz = 0, symbol = 0, word_num = 0; symbol <= line_sz; symbol++) {
+        if(line[symbol] == ' ' || line[symbol] == '\0') {
+            // malloc new sizeof(char*) each time and then malloc the appropriate amount for each word (+ 1 for null)
+            parsed[word_num] = malloc(word_sz + 1);
+            parsed = realloc(parsed, sizeof(char*) * (word_num + 2));
+            if(!parsed) {
+                free(line);
+                printf("Malloc error.");
+                exit(6);
+            }
+            if(!parsed[word_num]) {
+                for(int i = 0; i < word_num; i++) {
+                    free(parsed[i]);
+                }
+                free(parsed);
+                free(line);
+                printf("Malloc error.");
+                exit(6);
+            }
+            word_sz = 0; // :/ - i'm not doing <word_sz = -1> anymore :)
+            word_num++;
+        }
+        word_sz++;
+    }
+    int word = 0;
+    for(int symbol_in_line = 0, symbol_in_parsed = 0; symbol_in_line <= line_sz; symbol_in_line++) {
+        if(line[symbol_in_line] != ' ') {
+            parsed[word][symbol_in_parsed] = line[symbol_in_line];
+            symbol_in_parsed++;
+        }
+        else {
+            parsed[word][symbol_in_parsed] = '\0';
+            symbol_in_parsed = 0;
+            word++;
+        }
+    }
+    parsed[word + 1] = (char*)'\0';
+    free(line);
+    return parsed;
+}
+
+int main(int argc, char* argv[]) {
+    int fd = err_open(argv[1], O_RDONLY);
+    if(fd == -1) {
+        exit(8);
+    }
+
+    int num_lines = get_lines(fd);
+
+    char* line;
+
+    int check_lseek = err_lseek(fd, 0, SEEK_SET);
+    if(check_lseek == -1) {
+        exit(4);
+    }
+
+    char** parsed;
+
+    int pid, status, check_wait, check_exec;
+
+    for(int i = 0; i < num_lines; i++) {
+        pid = fork();
+        if(pid == 0) {
+            line = readline(fd);
+            parsed = parseline(line);
+            printf("%s\n", parsed[0]);
+
+            check_exec = execvp(parsed[0], parsed);
+            if(check_exec == -1) {
+                printf("Exec error.");
+                exit(12);
+            }
+        }
+        else {
+            check_wait = wait(&status);
+            if(check_wait == -1) {
+                printf("Wait error.");
+                exit(11);
+            }
+        }
+        if(WIFEXITED(status) == 1) {
+            printf("Exit status: %d\n", WEXITSTATUS(status));
+        }
+        else if(!WIFEXITED(status)){
+            printf("Process terminated.\n");
+            exit(10);
+        }
+    }
+}
